e0f177d68302ce318b341c32cd306501d9b9eb4e
Snehithahub <127884470+Snehithahub@users.noreply.github.com>
2023-04-09 19:54:31 +0530

    Add files via upload

---
+++ snehitha.js
@@ -0,0 +1,12 @@
+var display=function () {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter=='j') {
+            console.log("goodbye"+names[i]);
+           } else {
+            console.log("hii"+names[i]);
+           }
+       }
+       } 
+       display();
